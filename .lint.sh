12b009046103b9c0097eecec15123c2ff70d585c
Jeffrey H. Johnson <trnsz@pobox.com>
2025-10-07 17:45:17 -0400

    Add GCC Analyzer to linters
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- .lint.sh
+++ .lint.sh
@@ -16,6 +16,25 @@
 # shellcheck disable=SC2015,SC3040
 (set -o pipefail || :) > /dev/null 2>&1 && set -o pipefail > /dev/null 2>&1 || :
 
+xline()
+{
+  cols=$(tput cols 2> /dev/null || printf '%s\n' 80)
+
+  if [ "${cols:?}" -gt 999 ] || [ "${cols:?}" -lt 10 ]; then
+    cols=10
+  fi
+
+  line=
+  i=0
+
+  while [ "${i:?}" -lt "${cols:?}" ]; do
+    line="${line:-}="
+    i=$((i + 1))
+  done
+
+  printf '%s\n' "${line:?}"
+}
+
 set -eu
 
 # Cleanup
@@ -23,18 +42,25 @@ set -eu
 
 # Codespell
 CODESPELL="$(command -v codespell 2> /dev/null || printf '%s\n' true)"
-test "${CODESPELL:?}" != "true" && printf '%s\n' "Codespell..."
+test "${CODESPELL:?}" != "true" && {
+  xline
+  printf '%s\n' "Codespell..."
+}
 "${CODESPELL:?}" .
 
 # REUSE
 REUSE="$(command -v reuse 2> /dev/null || printf '%s\n' true)"
-
-test "${REUSE:?}" != "true" && printf '%s\n' "REUSE..."
+test "${REUSE:?}" != "true" && {
+  xline
+  printf '%s\n' "REUSE..."
+}
 "${REUSE:?}" lint -q || "${REUSE:?}" lint
+
 # SoftIntegration Ch C/C++ Interpreter
 CH="$(command -v ch 2> /dev/null || printf '%s\n' true)"
 test "${CH:?}" != "true" \
   && {
+    xline
     printf '%s\n' "Ch..."
     sed '1,3d' ./pc.c > ./ch.c
     ch ./ch.c "1+1" > /dev/null
@@ -43,14 +69,20 @@ test "${CH:?}" != "true" \
 
 # ShellCheck
 SHELLCHECK="$(command -v shellcheck 2> /dev/null || printf '%s\n' true)"
-test "${SHELLCHECK:?}" != "true" && printf '%s\n' "ShellCheck..."
+test "${SHELLCHECK:?}" != "true" && {
+  xline
+  printf '%s\n' "ShellCheck..."
+}
 "${SHELLCHECK:?}" -o any,all ./.lint.sh
 
 # Oracle Lint
 ORSTLINT="$(command -v /opt/developerstudio12.6/bin/lint 2> /dev/null \
   || command -v /opt/oracle/developerstudio12.6/bin/lint 2> /dev/null \
   || printf '%s\n' true)"
-test "${ORSTLINT:?}" != "true" && printf '%s\n' "Oracle Lint..."
+test "${ORSTLINT:?}" != "true" && {
+  xline
+  printf '%s\n' "Oracle Lint..."
+}
 "${ORSTLINT:?}" -std=c99 -err=warn -errtags=yes -errfmt=src \
   -errchk=structarg,longptr64,parentheses,locfmtchk \
   -errsecurity=extended -errshort=full -fd \
@@ -61,7 +93,10 @@ case "$(uname -s 2> /dev/null || :)" in
 NetBSD)
   {
     LINT="$(command -v /usr/bin/lint 2> /dev/null || printf '%s\n' true)"
-    test "${LINT:?}" != "true" && printf '%s\n' "NetBSD Lint..."
+    test "${LINT:?}" != "true" && {
+      xline
+      printf '%s\n' "NetBSD Lint..."
+    }
     lint -S -a -aa -b -c -e -g -h -P -r -u -z pc.c 2>&1 \
       | grep -Ev '(^lint: cannot find llib-lc\.ln$|^pc\.c:$)' || :
   }
@@ -73,6 +108,7 @@ esac
 command -v bear > /dev/null 2>&1 && {
   PVSSTUDIO="$(command -v pvs-studio-analyzer || printf '%s\n' true)"
   test "${PVSSTUDIO:?}" != "true" && {
+    xline
     printf '%s\n' "PVS-Studio..."
     "${MAKE:-make}" clean > /dev/null 2>&1
     rm -rf ./compile_commands.json ./log.pvs ./pvsreport
@@ -84,9 +120,24 @@ command -v bear > /dev/null 2>&1 && {
   }
 }
 
+# GCC Analyzer
+case "$(uname -s 2> /dev/null || :)" in
+Darwin) : ;; # GCC might not be real GCC on Darwin
+*) {
+  GCC="$(command -v gcc || printf '%s\n' true)"
+  test "${GCC:?}" != "true" && {
+    xline
+    printf '%s\n' "GCC Analyzer..."
+    "${GCC:?}" -fanalyzer -std=c99 -Wall -Wextra -Wpedantic -Werror -O3 pc.c -o pc.gcc
+    rm -f ./pc.gcc > /dev/null 2>&1
+  }
+} ;;
+esac
+
 # Clang Analyzer
 SCANBUILD="$(command -v scan-build 2> /dev/null || printf '%s\n' true)"
 test "${SCANBUILD:?}" != "true" && {
+  xline
   printf '%s\n' "Clang Analyzer..."
   "${SCANBUILD:?}" --status-bugs -maxloop 8 "${MAKE:-make}"
   "${MAKE:-make}" clean > /dev/null 2>&1
@@ -94,9 +145,15 @@ test "${SCANBUILD:?}" != "true" && {
 
 # Cppcheck
 CPPCHECK="$(command -v cppcheck 2> /dev/null || printf '%s\n' true)"
-test "${CPPCHECK:?}" != "true" && printf '%s\n' "Cppcheck..."
+test "${CPPCHECK:?}" != "true" && {
+  xline
+  printf '%s\n' "Cppcheck..."
+}
 "${CPPCHECK:?}" --inline-suppr --force --check-level=exhaustive \
   -DFREE=free -DHAS_INCLUDE=0 -D_DARWIN_C_SOURCE -D_GNU_SOURCE \
   -D_NETBSD_SOURCE -D_OPENBSD_SOURCE -D_POSIX_C_SOURCE=200809L \
   -D__BSD_VISIBLE=1 -UPAGESIZE -UPAGE_SIZE -U_PC_FILESIZEBITS \
   -D__EXTENSIONS__ --quiet pc.c
+
+# Final xline
+xline
